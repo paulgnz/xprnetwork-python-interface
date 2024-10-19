5b2e71337a7d16fda6847035c153be4e6754ddaa
Paul Grey <paulgnz@users.noreply.github.com>
2024-10-20 04:41:11 +1300

    fixes

--- examples/transfer.py
+++ examples/transfer.py
@@ -1,18 +1,27 @@
-"""Transfer some XPR to a receiver account."""
-
+import os
+from dotenv import load_dotenv
 import xprpy
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Fetch the private key from the environment
+key = os.getenv("XPRNETWORK_PRIVATE_KEY")
+
+if not key:
+    raise ValueError("XPRNETWORK_PRIVATE_KEY is not set in the environment")
+
 data = [
-    # In this case the account me.wam is transferring to account 'reciever'
+    # In this case, the account 'me.wam' is transferring to account 'receiver'
     xprpy.Data(name="from", value=xprpy.types.Name("me.wam")),
     xprpy.Data(name="to", value=xprpy.types.Name("receiver")),
     xprpy.Data(
-        name="quantity", # Selects the 'quantity' field in this action, must be a valid field in the action
-        value=xprpy.types.Asset("55.00000000 XPR"), # Asset type must be specified as 'quantity' requires the amount and currency type, which Asset includes
+        name="quantity",  # Selects the 'quantity' field in this action, must be a valid field in the action
+        value=xprpy.types.Asset("55.00000000 XPR"),  # Asset type must be specified as 'quantity' requires the amount and currency type, which Asset includes
     ),
     xprpy.Data(
-        name="memo", # Selects the 'memo' field in this action, just an extra message with the transfer
-        value=xprpy.types.String("Trying xprpy"), # String type is used for memo
+        name="memo",  # Selects the 'memo' field in this action, just an extra message with the transfer
+        value=xprpy.types.String("Trying xprpy"),  # String type is used for memo
     ),
 ]
 
@@ -27,10 +36,9 @@ action = xprpy.Action(
 
 raw_transaction = xprpy.Transaction(actions=[action])
 
-net = xprpy.XPRTestnet()
+net = xprpy.XPRMainnet()
 linked_transaction = raw_transaction.link(net=net)
 
-key = "a_very_secret_key"
 signed_transaction = linked_transaction.sign(key=key)
 
 resp = signed_transaction.send()
